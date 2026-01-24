4dddc4ae728c222c9f704ab32e68450bfeefe029
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-01-24 10:59:29 +0700

    feat(config): update jwt service and userdetail

---
+++ src/main/java/com/ntt/fintech_trading_backend/auth/security/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.ntt.fintech_trading_backend.auth.security;
+
+import com.ntt.fintech_trading_backend.auth.domain.User;
+import com.ntt.fintech_trading_backend.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new SecurityUser(user);
+    }
+}
