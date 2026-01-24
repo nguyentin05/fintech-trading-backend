289d3ded5b5a23bc450a06ba9a9b1211c07a32a4
Trong Tin Nguyen <220007781+nguyentin05@users.noreply.github.com>
2026-01-24 11:36:18 +0700

    feat(auth): update api login (#9)

---
+++ src/main/java/com/ntt/fintech_trading_backend/auth/dto/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.ntt.fintech_trading_backend.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không đúng định dạng")
+    private String email;
+
+    @NotBlank(message = "Mật khẩu không được để trống")
+    private String password;
+}
