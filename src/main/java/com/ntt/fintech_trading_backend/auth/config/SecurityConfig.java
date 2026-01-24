75ed180e58c48a7bb6a582567eaa2765fd045cd7
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-01-24 10:51:59 +0700

    feat(config): configurate security and add jwt filter

---
+++ src/main/java/com/ntt/fintech_trading_backend/auth/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.ntt.fintech_trading_backend.auth.config;
+
+import com.ntt.fintech_trading_backend.auth.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+    private static final String[] WHITE_LIST_URL = {
+            "/auth/**",
+            "/error"};
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                    .requestMatchers(WHITE_LIST_URL).permitAll()
+                    .anyRequest()
+                    .authenticated()
+                )
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class
+                )
+        ;
+
+        return http.build();
+    }
+}
